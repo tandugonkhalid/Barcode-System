5810d89f8353f67275682ee7aca0b59f2f0bdd4a
Khalid <67338442+tandugonkhalid@users.noreply.github.com>
2021-12-17 16:22:25 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barcode System</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="navbar">
+        <h1>Real Estate Investment Company</h1>
+        <button>Login</button>
+    </nav>
+    <section>
+        <div class="grid">
+            <img src="images/kamaladdin.jpg" alt="Apartments">
+            <img src="images/276724.jpg" alt="Apartments">
+            <img src="images/medhat-ayad.jpg" alt="Apartments">
+            <img src="images/271618.jpg" alt="Apartments">
+        </div>
+        <div class="info">
+            <h1>
+                "An online inventory system implemented with barcode using PHP and Sql Server for Real Estate Investment Company."
+            </h1>
+        </div>
+    </section>
+</body>
+</html>
