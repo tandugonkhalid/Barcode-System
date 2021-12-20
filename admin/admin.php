91fce61ffd364c97e908bde527020d3e48acb4a5
tandugonkhalid <khalidtandugon@gmail.com>
2021-12-20 23:19:42 +0300

    12/20/2021

---
+++ admin/admin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <title>Admin</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-primary">
+
+<!-- Links -->
+<ul class="navbar-nav">
+  <li class="nav-item">
+    <a class="nav-link text-dark" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-dark" href="#">Sign out</a>
+  </li>
+</ul>
+</nav>
+
+<div class="container d-flex justify-content-between align-items-center">
+    <div class="">
+        <h1>Add an account</h1>
+    </div>
+    <div class="">
+        <h1>View stock lists</h1>
+    </div>
+    <div class="">
+        <h1>Track appliances</h1>
+    </div>
+</div>
+</body>
+</html>
